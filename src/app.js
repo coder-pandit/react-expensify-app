726871d7811f27eecb29c608b81903e8f20d3e4e
shiv-k-sharma <kumarshivsharma12@gmail.com>
2018-09-14 15:23:41 +0530

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -5,21 +5,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-// getVisibleExpense -> print visible ones to screen
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-console.log(store.getState());
 
 // Provider from react-redux to provide redux store to connect with react
 const jsx = (
